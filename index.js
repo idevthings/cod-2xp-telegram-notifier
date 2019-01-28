9a0fffe68d1e311f2eb135cd362535ce06f77de0
Miguel Pérez <idevthings101@gmail.com>
2019-01-29 00:01:00 +0100

    default port asigned to environment (heroku doesnt allow to assign fixed port number)

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get('/', function (req, res) {
     res.send('CoD notifier!');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Example app listening on port 3000!');
 });
